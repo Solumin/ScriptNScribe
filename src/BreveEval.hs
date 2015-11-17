7ba045c0e8b1f6b154a8c10179417bf005757c88
Teddy Sudol <magesolumin@gmail.com>
2015-11-17 18:40:59 -0500

    Working on eval of simple progs

--- src/BreveEval.hs
+++ src/BreveEval.hs
@@ -1,2 +1,32 @@
 import BreveLang
-import Euterpea
+import qualified Euterpea.Music.Note.Music as E hiding (Note)
+import qualified Euterpea as E (play, line)
+import Text.Parsec (parse)
+
+type Music = E.Music E.Pitch
+
+eval :: Statement -> Music
+eval (Seq (s:ss)) = evalStatement s
+
+evalStatement :: Statement -> Music
+evalStatement (v := n) = evalNote n
+
+evalNote :: Expr -> Music
+evalNote (Note (PitchClass p) (Octave o) (Duration d)) = E.note d (p, o)
+
+evalRest :: Expr -> Music
+evalRest (Rest (Duration d)) = E.rest d
+
+evalSnippet :: Expr -> Music
+evalSnippet (Snippet ss) = E.line (map body ss)
+    where
+        body :: Expr -> Music
+        body n@(Note p o d) = evalNote n
+        body r@(Rest d) = evalRest r
+        body _ = error "Snippet can only contain Notes and Rests"
+
+
+parseEval :: String -> IO()
+parseEval input = case parse breveParser "input" input of
+    Left err -> error (show err)
+    Right statement -> E.play (eval statement)
