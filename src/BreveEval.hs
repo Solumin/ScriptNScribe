4540899eabfb79d66007f7dfc8a5205b102b7483
Solumin <magesolumin@gmail.com>
2015-11-27 17:49:45 -0500

    Eval actually works??

--- src/BreveEval.hs
+++ src/BreveEval.hs
@@ -30,16 +30,19 @@ interp input = let (prog,_) = parse input in
     interpStatement [] prog
 
 -- Takes source code and turns it into a single musical phrase
-eval :: String -> Music
-eval input =
-    let env = interp input in
-    case lookup "main" env of
-        Just main -> evalMain env main
-        Nothing -> error "No main in program."
+-- eval :: String -> Music
+-- eval input =
+--     let env = interp input in
+--     case lookup "main" env of
+--         Just main -> evalMain env main
+--         Nothing -> error "No main in program."
 
 -- Takes source code and performs the music described in it
-perform :: String -> IO()
-perform = Euterpea.play . eval
+-- perform :: String -> IO()
+-- perform = Euterpea.play . eval
+
+parseEval :: String -> Val
+parseEval = eval . interp
 
 -- ==========
 -- Interpret
@@ -78,86 +81,29 @@ interpExpr env (App s es) = App s (map (interpExpr env) es)
 -- Evaluating
 -- ==========
 
-eval :: Env -> Env
-eval env = eval' env []
+-- Attempt 4: Val again (repeat attempt 1, really)
 
--- Generates a new environment by shifting all the old bindings to the new
--- environment, eval'ing as it goes
-eval' :: Env -> Env -> Env
-eval' [] new = new
-eval' ((s,e):old) new = eval' old (s, evalExpr old new e):new
-
-evalExpr :: Env -> Env -> Expr -> Expr
-evalExpr old new exp = let evalE = evalExpr old new in
-     case expr of
-    -- Basic values don't change
-    p@(PitchClass _ _) -> p
-    n@(N _ _) -> n
-    d@(D _ _) -> d
-    b@(B _) -> b
-    -- Notes, Rests and Snippets need to be type checked
-    (Note p o d) -> note (evalE p) (evalE o) (evalE d)
-    (Rest d) -> rest (evalE d)
-    (Snippet ss) -> snippet (map evalE ss)
-    -- Lists don't need to be type checked but they should be evaluated
-    (List ls) -> List (map evalE ls)
-    -- Lambdas don't need to be processed?
-    l@(Lambda _ _) -> l
-    -- The following expressions need to be evaluated to a new result!
-    (UnOpExpr op e) -> evalUnOp op (evalE e)
-    (BinOpExpr op e1 e2) -> evalBinOp op (evalE e1) (evalE e2)
-    (Var s) -> lookupVar old new s
-    (App s args) -> evalApp old new s (map evalE args)
-
--- Notes absolutely must have a PitchClas and an Integer for the first two
--- arguments. The third argument may be an Integer or a Double.
-note :: Expr -> Expr -> Expr -> Expr
-note p@(PitchClass _ _) o@(N _ _) dur = Note p o (checkDur dur)
-note _ _ _ = error "A Note requires a PitchClass, an Integer and a number"
-
--- Rest has the same duration requirements as Note
-rest :: Expr -> Expr
-rest = Rest . checkDur
-
-checkDur :: Expr -> Expr
-checkDur d@(Vd _ _) = d
-checkDur n@(Vn _ _) = n
-checkDur _ = error "A duration must be a number (Integer or Double)"
-
--- Snippets must have only rests and notes
-snippet :: [Expr] -> Expr
-snippet ss = Snippet (map checkFun ss)
-    where
-    checkFun s = case s of
-        (Note _ _ _) -> s
-        (Rest _ )    -> s
-        _ -> error ("Snippet expects Note or Rest, but received " ++ show s)
-
-lookupVar :: Env -> Env -> String -> Expr
-lookupVar old new v = fromMaybe (
-    fromMaybe (error $ "Unknown variable " ++ show v) (lookup v old)
-    ) (lookup v new)
-
-evalApp :: Env -> Env -> String -> [String] -> Expr
-
-{-
--- Main can validly be a note, a rest or a snippet.
--- Notes and Rests are lifted to Snippets automatically.
-evalMain :: Env -> Expr -> Music
-evalMain env n@(Note{}) = evalMain env (Snippet [n])
-evalMain env r@(Rest _) = evalMain env (Snippet [r])
-evalMain env s@(Snippet _) = let (Vm m) = evalExpr env s in m
-evalMain _ _ = error "Main must be a Snippet (or a Note or Rest)"
-
-data Val = Vp E.PitchClass | Vn Integer | Vd Double | Vb Bool | Vm Music | VList [Val]
+data Val = Vp E.PitchClass
+         | Vn Integer | Vd Double
+         | Vb Bool
+         | Ve Music | Vr Music -- Note and Rest, respectively
+         | Vs Val Val -- represents :+: and Snippet
+         | Vo Val Val -- represents the :=: operator for rest, notes and snippets
+         | Vl [Val]      -- Expr List
+         | Vf FunDef
+data FunDef = FunDef [String] Env -- lambda eval'd
 
 instance Show Val where
     show (Vp p) = "PitchClass <" ++ (shows p ">")
     show (Vn n) = "Integer <" ++ (shows n ">")
     show (Vd d) = "Double <" ++ (shows d ">")
     show (Vb b) = show b
-    show (Vm m) = "Music"
-    show (VList l) = "Vlist [" ++ concat (intersperse ", " (map show l)) ++ "]"
+    show (Ve e) = show e
+    show (Vr r) = show r
+    show (Vs a b) = shows a " :+: " ++ show b
+    show (Vo a b) = shows a " :=: " ++ show b
+    show (Vl l) = "Vlist [" ++ concat (intersperse ", " (map show l)) ++ "]"
+    show (Vf (FunDef args bins)) = "Function <" ++ shows args ">"
 
 instance Eq Val where
     (Vp a) == (Vp b) = a == b
@@ -166,8 +112,11 @@ instance Eq Val where
     (Vd a) == (Vn b) = (Vn b) == (Vd a)
     (Vd a) == (Vd b) = a == b
     (Vb a) == (Vb b) = a == b
-    (Vm a) == (Vm b) = a == b
-    (VList a) == (VList b) = a == b
+    (Ve a) == (Ve b) = a == b
+    (Vr a) == (Vr b) = a == b
+    (Vs a b) == (Vs c d) = a == c && b == d
+    (Vo a b) == (Vo c d) = a == c && b == d
+    (Vl a) == (Vl b) = a == b
     a == b = error $ unwords ["Cannot compare", show a, "and", show b]
 
 instance Ord Val where
@@ -177,75 +126,48 @@ instance Ord Val where
     (Vd a) <= (Vn b) = a <= (fromInteger b)
     (Vd a) <= (Vd b) = a <= b
     (Vb a) <= (Vb b) = a <= b
-    (Vm a) <= (Vm b) = a <= b
-    (VList a) <= (VList b) = a <= b
+    (Ve a) <= (Ve b) = a <= b
+    (Vr a) <= (Vr b) = a <= b
+    (Vo a b) <= (Vo c d) = a <= c && b <= d
+    (Vs a b) <= (Vs c d) = a <= c && b <= d
+    (Vl a) <= (Vl b) = a <= b
     a <= b = error $ unwords ["Cannot compare", show a, "and", show b]
 
+bindings :: Statement -> Env
+bindings (Seq ss) = map bind ss
+    where bind (Assign n e) = (n, e)
+          bind (Return e) = ("return", e)
+
+eval :: Env -> Val
+eval env = evalBinding env "main"
+
+evalBinding :: Env -> String -> Val
+evalBinding env name = evalExpr env $ fromMaybe (error $ shows name " is not defined.") (lookup name env)
+
 evalExpr :: Env -> Expr -> Val
-evalExpr env (PitchClass p _) = Vp p
-evalExpr env (N n _) = Vn n
-evalExpr env (D d _) = Vd d
-evalExpr env (B b) = Vb b
-evalExpr env (UnOpExpr op e) = evalUnOp op (evalExpr env e)
-evalExpr env (BinOpExpr op e1 e2) =
-    evalBinOp op (evalExpr env e1) (evalExpr env e2)
-evalExpr env (Note p o d) =
-    note (evalExpr env p) (evalExpr env o) (evalExpr env d)
-evalExpr env (Rest d) = rest (evalExpr env d)
-evalExpr env (Snippet ss) = Vm $ line (map (evalExpr env) ss)
-evalExpr env (Var v) = case lookup v env of
-    Just e -> evalExpr env e
-    Nothing -> error ("Unknown variable " ++ v)
-evalExpr env (List ls) = VList (map (evalExpr env) ls)
-
-evalUnOp :: UnOp -> Val -> Val
-evalUnOp Not (Vb b) = Vb (not b)
-evalUnOp Not _ = error "Not takes boolean"
-
--- There's got to be a better way
-evalBinOp :: BinOp -> Val -> Val -> Val
-
-evalBinOp Add (Vd d1) (Vd d2) = Vd (d1 + d2)
-evalBinOp Add (Vn n1) (Vn n2) = Vn (n1 + n2)
--- Using the behavior of Euterpea's pitch and trans functions, which always
--- return a Sharp note instead of a flat enharmonic.
-evalBinOp Add (Vp pc) (Vn n) = Vp (pitches !! ((E.pcToInt pc + d) `mod` 12))
-    where
-        pitches = [E.C, E.Cs, E.D, E.Ds, E.E, E.F, E.Fs, E.G, E.Gs, E.A, E.As, E.B]
-        d = fromInteger n
-evalBinOp Add d@(Vn n) pc@(Vp p) = evalBinOp Add pc d
-
-evalBinOp Sub (Vd d1) (Vd d2) = Vd (d1 - d2)
-evalBinOp Sub (Vn n1) (Vn n2) = Vn (n1 - n2)
-evalBinOp Sub (Vp pc) (Vn n) = evalBinOp Add (Vp pc) (Vn (negate n))
-evalBinOp Sub (Vn n) (Vp pc) = evalBinOp Add (Vp pc) (Vn (negate n))
-
-evalBinOp Mult (Vd d1) (Vd d2) = Vd (d1 * d2)
-evalBinOp Mult (Vn n1) (Vn n2) = Vn (n1 * n2)
-
--- Note that Div always returns a double!!
-evalBinOp Div (Vd d1) (Vd d2) = Vd (d1 / d2)
-evalBinOp Div (Vn n1) (Vn n2) = Vd (fromInteger n1 / fromInteger n2)
-
-evalBinOp SeqOp (Vm m1) (Vm m2) = Vm (m1 E.:+: m2)
-evalBinOp ParOp (Vm m1) (Vm m2) = Vm (m1 E.:=: m2)
-
-evalBinOp Eq  a b = Vb (a == b)
-evalBinOp Neq a b = Vb (a /= b)
-evalBinOp Lt  a b = Vb (a <  b)
-evalBinOp Lte a b = Vb (a <= b)
-evalBinOp Gt  a b = Vb (a >  b)
-evalBinOp Gte a b = Vb (a >= b)
-
-evalBinOp o a b = error $ unwords ["Op", show o, "is undefined for args", show a, show b]
+evalExpr env expr = let evalE = evalExpr env in
+    case expr of
+    (PitchClass p _) -> Vp p
+    (N n _) -> Vn n
+    (D d _) -> Vd d
+    (B b) -> Vb b
+    (UnOpExpr op e) -> evalUnOp op (evalExpr env e)
+    (BinOpExpr op e1 e2) -> evalBinOp op (evalExpr env e1) (evalExpr env e2)
+    (Note p o d) -> note (evalE p) (evalE o) (evalE d)
+    (Rest d) -> rest (evalE d)
+    (Snippet ss) -> snippet (map evalE ss)
+    (List ls) -> Vl (map evalE ls)
+    (Var s) -> evalE (lookupVar env s)
+    (Lambda params body) -> evalFunc params body
+    (App name args) -> evalApp env name args
 
 note :: Val -> Val -> Val -> Val
-note (Vp p) o d = Vm $ E.note (valToDur d) (p, valToOct o)
+note (Vp p) o d = Ve $ E.note (valToDur d) (p, valToOct o)
 note _ _ _ = error "Note takes a pitch class, an octave and a duration"
 
 rest :: Val -> Val
-rest (Vd d) = Vm (E.rest (toRational d))
-rest (Vn n) = Vm (E.rest (toRational n))
+rest (Vd d) = Vr (E.rest (toRational d))
+rest (Vn n) = Vr (E.rest (toRational n))
 rest _ = error "Rests take a duration"
 
 valToDur :: Val -> E.Dur
@@ -257,9 +179,85 @@ valToOct :: Val -> E.Octave
 valToOct (Vn n) = fromInteger n
 valToOct _ = error "Octaves must be integers"
 
-line :: [Val] -> Music
-line [] = E.rest 0
-line [Vm m] = m
-line (Vm m : vs) = m E.:+: line vs
-line _ = error "Expected music in the snippet"
--}
+snippet :: [Val] -> Val
+snippet (v@(Ve _):vs) = case vs of
+    [] -> v
+    _ -> Vs v (snippet vs)
+snippet (v@(Vr _):vs) = case vs of
+    [] -> v
+    _ -> Vs v (snippet vs)
+snippet (_:vs) = error "A snippet should only contain Note or Rest objects"
+
+lookupVar :: Env -> String -> Expr
+lookupVar env name = fromMaybe (error $ shows name " is undefined.") (lookup name env)
+
+evalFunc :: [String] -> Statement -> Val
+evalFunc params = Vf . FunDef params . bindings
+
+evalApp :: Env -> String -> [Expr] -> Val
+evalApp env name args =
+    let (Lambda p s) = lookupVar env name
+        (Vf (FunDef params binds)) = evalFunc p s in
+    evalBinding (binds ++ zip params args ++ env) "return"
+
+evalUnOp :: UnOp -> Val -> Val
+evalUnOp Not v = case v of
+    (Vb b) -> Vb (not b)
+    _ -> error "\"Not\" takes boolean"
+
+evalBinOp :: BinOp -> Val -> Val -> Val
+
+evalBinOp Add (Vd d1) (Vd d2) = Vd (d1 + d2)
+evalBinOp Add (Vd d1) (Vn n2) = Vd (d1 + fromInteger n2)
+evalBinOp Add (Vn n1) (Vd d2) = Vd (fromInteger n1 + d2)
+evalBinOp Add (Vn n1) (Vn n2) = Vn (n1 + n2)
+-- Using the behavior of Euterpea's pitch and trans functions, which always
+-- return a sharp note instead of a flat enharmonic.
+evalBinOp Add (Vp pc) (Vn n) = Vp (pitches !! ((E.pcToInt pc + d) `mod` 12))
+    where
+        pitches = [E.C, E.Cs, E.D, E.Ds, E.E, E.F, E.Fs, E.G, E.Gs, E.A, E.As, E.B]
+        d = fromInteger n
+
+evalBinOp Sub (Vd d1) (Vd d2) = Vd (d1 - d2)
+evalBinOp Sub (Vd d1) (Vn n2) = Vd (d1 - fromInteger n2)
+evalBinOp Sub (Vn n1) (Vd d2) = Vd (fromInteger n1 - d2)
+evalBinOp Sub (Vn n1) (Vn n2) = Vn (n1 - n2)
+evalBinOp Sub (Vp pc) (Vn n) = evalBinOp Add (Vp pc) (Vn (negate n))
+
+evalBinOp Mult (Vd d1) (Vd d2) = Vd (d1 * d2)
+evalBinOp Mult (Vd d1) (Vn n2) = Vd (d1 * fromInteger n2)
+evalBinOp Mult (Vn n1) (Vd d2) = Vd (fromInteger n1 * d2)
+evalBinOp Mult (Vn n1) (Vn n2) = Vn (n1 * n2)
+
+-- Note that Div always returns a double!!
+evalBinOp Div (Vd d1) (Vd d2) = Vd (d1 / d2)
+evalBinOp Div (Vd d1) (Vn n2) = Vd (d1 / fromInteger n2)
+evalBinOp Div (Vn n1) (Vd d2) = Vd (fromInteger n1 / d2)
+evalBinOp Div (Vn n1) (Vn n2) = Vd (fromInteger n1 / fromInteger n2)
+
+evalBinOp SeqOp a b = Vs (check a) (check b)
+    where
+    check v = case v of
+        (Ve _) -> v
+        (Vr _) -> v
+        (Vs _ _) -> v
+        (Vo _ _) -> v
+        _ -> error $ ":+: is undefined for argument " ++ show v
+
+evalBinOp ParOp a b = Vo (check a) (check b)
+    where
+    check v = case v of
+        (Ve _) -> v
+        (Vr _) -> v
+        (Vs _ _) -> v
+        (Vo _ _) -> v
+        _ -> error $ ":=: is undefined for argument " ++ show v
+
+evalBinOp Eq  a b = Vb (a == b)
+evalBinOp Neq a b = Vb (a /= b)
+evalBinOp Lt  a b = Vb (a <  b)
+evalBinOp Lte a b = Vb (a <= b)
+evalBinOp Gt  a b = Vb (a >  b)
+evalBinOp Gte a b = Vb (a >= b)
+
+evalBinOp o a b = error $ unwords ["Op", show o, "is undefined for args", show a, show b]
