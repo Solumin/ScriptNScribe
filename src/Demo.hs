46b33ec518c40b7f915d1d99c42a893cb0b3bd13
Solumin <magesolumin@gmail.com>
2015-12-07 11:43:22 -0500

    Updated demo for new synth

--- src/Demo.hs
+++ src/Demo.hs
@@ -19,7 +19,7 @@ import Data.Maybe (fromJust)
 
 fibo = "fibo = (\\ n -> if n <= 1 then n else fibo(n-1) + fibo(n-2)); main = fibo(5);"
 
-program = "dfsa = arpeggio([0,4,7], (D 4 1/2)); main = line(dfsa) :+: (rest 1/4) :+: chord(dfsa);"
+program = "dfsa = arpeggio([0,4,7], (D 4 1/2));\nmain = line(dfsa) :+: (rest 1/4) :+: chord(dfsa);"
 
 states = fst $ parse program
 
@@ -28,16 +28,34 @@ traces = snd $ parseEval program
 showAll :: Show a => [a] -> IO ()
 showAll = mapM_ (putStrLn . show)
 
-update = [Vp (read "F") (TrOp Add (TrLoc (1,29)) (TrLoc (1,21)))]
+update = [Vp (read "F") (TrOp Add (TrLoc (1,29)) (TrLoc (1,21)))
+         ,Vn 4 (TrLoc (1,34))]
 
-synthed = fromJust $ synthFaithful (toTraceMap traces) update
 
-newprog = show $ updateProgram synthed states
+synthed = synthFaithful (toTraceMap traces) update
 
-states' = fst $ parse newprog
+newprog = map (show . (`updateProgram` states)) synthed
 
-traces' = snd $ parseEval newprog
+states' = map (fst . parse) newprog
+
+traces' = map (snd . parseEval) newprog
 
 undo = [Vp (read "Fs") (TrOp Add (TrLoc (1,32)) (TrLoc (1,22)))]
 
-restored = show $ (flip updateProgram) states' $ fromJust $ synthFaithful (toTraceMap traces') undo
+restored = map show $ map (`updateProgram` (head states')) $ synthFaithful (toTraceMap $ head traces') undo
+
+main = do
+    putStrLn program
+    getLine
+    perform program
+    getLine
+    showAll traces
+    getLine
+    putStrLn $ show update
+    getLine
+    mapM_ (\p -> putStrLn p >> getLine >> perform p >> getLine) newprog
+    putStrLn program
+    getLine
+    putStrLn $ show undo
+    getLine
+    mapM_ (\p -> putStrLn p >> getLine >> perform p >> getLine) restored
