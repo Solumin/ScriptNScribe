b678013ee27e1f70f5dd246f408eff82f8829c90
Teddy Sudol <magesolumin@gmail.com>
2015-12-13 22:10:06 -0500

    Added basic playback. Need to do syncing

--- src/UI/BreveUI.hs
+++ src/UI/BreveUI.hs
@@ -1,3 +1,7 @@
+import qualified BreveEval
+
+import Control.Monad.IO.Class (liftIO)
+
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
@@ -8,30 +12,36 @@ setup :: Window -> UI ()
 setup window = do
     return window # set UI.title "Script-n-Scribe"
     code <- codebox
-    code2 <- codebox
+    code2 <- codebox # set UI.value "Music goes here..."
+
+    sync <- controlButton "Sync"
+    play <- controlButton "Play"
+
+    controls <- UI.div #. "controls" #+ map element [sync, play]
+
     mainbox <- UI.div #. "container" #+
         [ element code
-        , controls
+        , element controls
         , element code2
         ]
+
     getBody window #+
         [ UI.div #. "header" #+ [string "Script-N-Scribe"]
         , element mainbox
         ]
-    debug "Done"
 
--- For some reason threepenny doesn't have HTML attributes. We can make our own
--- pretty easily though!
-wrap :: WriteAttr Element String
-wrap = UI.mkWriteAttr (UI.set' (UI.attr "wrap"))
+    on UI.click sync $ const $ do
+        source <- get UI.value code
+        element code2 # set UI.value source
+
+    on UI.click play $ const $ do
+        source <- get UI.value code
+        liftIO (BreveEval.perform source)
 
 codebox :: UI Element
 codebox = do
-    input <- UI.textarea #. "codebox" # set wrap "off"
+    input <- UI.textarea #. "codebox" # set (UI.attr "wrap") "off"
     return input
 
-controls :: UI Element
-controls = do
-    butt <- UI.button #. "button1" # set UI.text "SDKFJDS"
-    cont <- (UI.div #. "controls") #+ [ element butt ]
-    return cont
+controlButton :: String -> UI Element
+controlButton name = UI.button #. "controlbutton" # set UI.text name
